a953f02901e1ae310dc9d2b3cf85129c5090c207
zhengwei <zhengwei@shimo.im>
2020-12-31 17:05:11 +0800

    fix: ego compatibility

--- egorm/options.go
+++ egorm/options.go
@@ -4,9 +4,10 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"time"
+
 	"github.com/gotomicro/ego/core/eapp"
 	"github.com/gotomicro/ego/core/util/xdebug"
-	"time"
 
 	"github.com/gotomicro/ego/core/elog"
 	"github.com/gotomicro/ego/core/emetric"
@@ -27,9 +28,13 @@ func debugInterceptor(compName string, dsn *DSN, op string, options *Config, log
 			cost := time.Since(beg)
 			if eapp.IsDevelopmentMode() {
 				if scope.HasError() {
-					xdebug.Error(compName, fmt.Sprintf("%v", dsn.Addr+"/"+dsn.DBName), cost, logSQL(scope.SQL, scope.SQLVars, true), scope.DB().Error.Error())
+					logger.Error("egorm.reply", elog.String("msg",
+						xdebug.MakeReqResError(compName, fmt.Sprintf("%v", dsn.Addr+"/"+dsn.DBName), cost, logSQL(scope.SQL, scope.SQLVars, true), scope.DB().Error.Error())),
+					)
 				} else {
-					xdebug.Info(compName, fmt.Sprintf("%v", dsn.Addr+"/"+dsn.DBName), cost, logSQL(scope.SQL, scope.SQLVars, true), fmt.Sprintf("%v", scope.Value))
+					logger.Info("egorm.reply", elog.String("msg",
+						xdebug.MakeReqResInfo(compName, fmt.Sprintf("%v", dsn.Addr+"/"+dsn.DBName), cost, logSQL(scope.SQL, scope.SQLVars, true), fmt.Sprintf("%v", scope.Value))),
+					)
 				}
 			} else {
 				// todo log debug info
