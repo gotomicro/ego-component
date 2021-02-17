eca20d3eba260a33d9e5e775c8a3d34c87583166
Ming Deng <mindeng@ebay.com>
2021-02-17 18:22:45 +0800

    refactor to Container

--- ewechat/config.go
+++ ewechat/config.go
@@ -1,19 +1,6 @@
 package wechat
 
-import (
-	"github.com/go-resty/resty/v2"
-	"github.com/gotomicro/ego/core/econf"
-	"github.com/gotomicro/ego/core/elog"
-
-	"github.com/gotomicro/ego-component/eredis"
-	"github.com/gotomicro/ego-component/ewechat/cache"
-	"github.com/gotomicro/ego-component/ewechat/context"
-	"github.com/gotomicro/ego-component/ewechat/miniprogram"
-
-	"sync"
-)
-
-type Option func(c *Config)
+type Option func(c *Container)
 
 // ModName ..
 const ModName = "contrib.wechat"
@@ -28,60 +15,10 @@ type Config struct {
 	PayMchID       string
 	PayKey         string
 	PayNotifyURL   string
-
-	Context *context.Context
-	client  cache.Cache
-	logger  *elog.Component
 }
 
 // DefaultConfig ...
 func DefaultConfig() *Config {
 	return &Config{
-		logger: elog.EgoLogger.With(elog.FieldComponent(ModName)),
 	}
-}
-
-// Invoker ...
-func Load(key string) *Config {
-	var config = DefaultConfig()
-	if err := econf.UnmarshalKey(key, &config); err != nil {
-		config.logger.Panic("parse wechat config panic", elog.FieldErr(err), elog.FieldKey(key), elog.FieldValueAny(config))
-	}
-	config.logger = config.logger.With(elog.FieldComponentName(key))
-	return config
-}
-
-// Build
-func (cfg *Config) Build(options ...Option) *Config {
-	ctx := new(context.Context)
-	ctx.AppID = cfg.AppID
-	ctx.AppSecret = cfg.AppSecret
-	ctx.Token = cfg.Token
-	ctx.EncodingAESKey = cfg.EncodingAESKey
-	ctx.PayMchID = cfg.PayMchID
-	ctx.PayKey = cfg.PayKey
-	ctx.PayNotifyURL = cfg.PayNotifyURL
-	cfg.Context = ctx
-	ctx.SetAccessTokenLock(new(sync.RWMutex))
-	ctx.SetJsAPITicketLock(new(sync.RWMutex))
-	ctx.RestyClient = resty.New().SetDebug(cfg.Debug)
-
-	for _, option := range options {
-		option(cfg)
-	}
-
-	ctx.Cache = cfg.client
-
-	return cfg
-}
-
-func WithRedis(client *eredis.Component) Option {
-	return func(c *Config) {
-		c.client = client
-	}
-}
-
-// GetMiniProgram 获取小程序的实例
-func (cfg *Config) GetMiniProgram() *miniprogram.MiniProgram {
-	return miniprogram.NewMiniProgram(cfg.Context)
 }
---
+++ ewechat/container.go
@@ -0,0 +1,91 @@
+// Copyright 2020 
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package wechat
+
+import (
+	"sync"
+
+	"github.com/go-resty/resty/v2"
+	"github.com/gotomicro/ego-component/eredis"
+	"github.com/gotomicro/ego/core/econf"
+	"github.com/gotomicro/ego/core/elog"
+
+	"github.com/gotomicro/ego-component/ewechat/cache"
+	"github.com/gotomicro/ego-component/ewechat/context"
+	"github.com/gotomicro/ego-component/ewechat/miniprogram"
+)
+
+type Container struct {
+	config *Config
+	name string
+	Context *context.Context
+	client  cache.Cache
+	logger  *elog.Component
+}
+
+func DefaultContainer() *Container {
+	return &Container{
+		config: DefaultConfig(),
+		logger: elog.EgoLogger.With(elog.FieldComponent(ModName)),
+	}
+}
+
+
+// Invoker ...
+func Load(key string) *Container {
+	container := DefaultContainer()
+	if err := econf.UnmarshalKey(key, container.config); err != nil {
+		container.logger.Panic("parse wechat config panic",
+			elog.FieldErr(err), elog.FieldKey(key), elog.FieldValueAny(container.config))
+	}
+	container.logger = container.logger.With(elog.FieldComponentName(key))
+	return container
+}
+
+// Build
+func (con *Container) Build(options ...Option) *Container {
+	cfg := con.config
+	ctx := new(context.Context)
+	ctx.AppID = cfg.AppID
+	ctx.AppSecret = cfg.AppSecret
+	ctx.Token = cfg.Token
+	ctx.EncodingAESKey = cfg.EncodingAESKey
+	ctx.PayMchID = cfg.PayMchID
+	ctx.PayKey = cfg.PayKey
+	ctx.PayNotifyURL = cfg.PayNotifyURL
+	con.Context = ctx
+	ctx.SetAccessTokenLock(new(sync.RWMutex))
+	ctx.SetJsAPITicketLock(new(sync.RWMutex))
+	ctx.RestyClient = resty.New().SetDebug(cfg.Debug)
+
+	for _, option := range options {
+		option(con)
+	}
+
+	ctx.Cache = con.client
+
+	return con
+}
+
+func WithRedis(client *eredis.Component) Option {
+	return func(c *Container) {
+		c.client = client
+	}
+}
+
+// GetMiniProgram 获取小程序的实例
+func (con *Container) GetMiniProgram() *miniprogram.MiniProgram {
+	return miniprogram.NewMiniProgram(con.Context)
+}
