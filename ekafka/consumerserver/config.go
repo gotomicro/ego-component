aeb3297589a60111cfc82a77aff32529c6679931
AaronJan <aaronjan@qq.com>
2021-03-12 10:34:16 +0800

    fix: add config tags

--- ekafka/consumerserver/config.go
+++ ekafka/consumerserver/config.go
@@ -3,8 +3,8 @@ package consumerserver
 import "github.com/gotomicro/ego-component/ekafka"
 
 type config struct {
-	Debug           bool `json:"debug" toml:"debug"`
-	ConsumerName    string
+	Debug           bool   `json:"debug" toml:"debug"`
+	ConsumerName    string `json:"consumerName" toml:"consumerName"`
 	ekafkaComponent *ekafka.Component
 }
 
