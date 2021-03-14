c0897ca5a401312d123de5104c367c59ac1b9413
AaronJan <aaronjan@qq.com>
2021-03-14 23:06:57 +0800

    feat: 支持全手动消费模式 & 优化API设计

--- ekafka/consumerserver/component.go
+++ ekafka/consumerserver/component.go
@@ -21,20 +21,22 @@ const PackageName = "component.ekafka.consumerserver"
 type consumptionMode int
 
 const (
-	consumptionModeSingle consumptionMode = iota + 1
+	consumptionModeManual consumptionMode = iota + 1
+	consumptionModeSingle
 )
 
 // Component starts an Ego server for message consuming.
 type Component struct {
-	config               *config
-	name                 string
-	ekafkaComponent      *ekafka.Component
-	logger               *elog.Component
-	serverCtx            context.Context
-	stopServer           context.CancelFunc
-	mode                 consumptionMode
-	singleMessageHandler SingleMessageHandler
-	consumptionErrors    chan error
+	ServerCtx             context.Context
+	stopServer            context.CancelFunc
+	config                *config
+	name                  string
+	ekafkaComponent       *ekafka.Component
+	logger                *elog.Component
+	mode                  consumptionMode
+	onEachMessageHandler  OnEachMessageHandler
+	onStartMessageHandler OnStartHandler
+	consumptionErrors     chan<- error
 }
 
 // PackageName returns the package name.
@@ -75,6 +77,8 @@ func (cmp *Component) Name() string {
 // Start will start consuming.
 func (cmp *Component) Start() error {
 	switch cmp.mode {
+	case consumptionModeManual:
+		return cmp.startManualMode()
 	case consumptionModeSingle:
 		return cmp.startSingleMode()
 	default:
@@ -87,53 +91,125 @@ func (cmp *Component) GetConsumer() *ekafka.Consumer {
 	return cmp.ekafkaComponent.Consumer(cmp.config.ConsumerName)
 }
 
-// EachMessage registers a single message handler.
-func (cmp *Component) EachMessage(consumptionErrors chan error, handler SingleMessageHandler) error {
+// OnEachMessage registers a single message handler.
+func (cmp *Component) OnEachMessage(consumptionErrors chan<- error, handler OnEachMessageHandler) error {
 	cmp.consumptionErrors = consumptionErrors
 	cmp.mode = consumptionModeSingle
-	cmp.singleMessageHandler = handler
+	cmp.onEachMessageHandler = handler
 	return nil
+}
+
+// OnStart registers a manual message handler.
+func (cmp *Component) OnStart(handler OnStartHandler) error {
+	cmp.mode = consumptionModeManual
+	cmp.onStartMessageHandler = handler
+	return nil
+}
+
+func isErrorUnrecoverable(err error) bool {
+	if kafkaError, ok := err.(kafka.Error); ok {
+		if kafkaError.Temporary() {
+			return true
+		}
+	}
+	return false
+}
+
+func (cmp *Component) startManualMode() error {
+	consumer := cmp.GetConsumer()
+
+	if cmp.onStartMessageHandler == nil {
+		return errors.New("you must define a MessageHandler first")
+	}
+
+	handlerExit := make(chan error)
+	go func() {
+		handlerExit <- cmp.onStartMessageHandler(cmp.ServerCtx, consumer)
+		close(handlerExit)
+	}()
+
+	var originErr error
+	select {
+	case originErr := <-handlerExit:
+		if originErr != nil {
+			cmp.logger.Error("terminating consumer because an error", elog.FieldErr(originErr))
+		} else {
+			cmp.logger.Info("message handler exited without any error, terminating consumer server")
+		}
+		cmp.stopServer()
+	case <-cmp.ServerCtx.Done():
+		originErr := cmp.ServerCtx.Err()
+		cmp.logger.Error("terminating consumer because a context error", elog.FieldErr(originErr))
+
+		err := <-handlerExit
+		if err != nil {
+			cmp.logger.Error("terminating consumer because an error", elog.FieldErr(err))
+		} else {
+			cmp.logger.Info("message handler exited without any error")
+		}
+	}
+
+	err := cmp.closeConsumer(consumer)
+	if err != nil {
+		return fmt.Errorf("encountered an error while closing consumer: %w", err)
+	}
+
+	if errors.Is(originErr, context.Canceled) {
+		return nil
+	}
+
+	return originErr
 }
 
 func (cmp *Component) startSingleMode() error {
 	consumer := cmp.GetConsumer()
 
-	if cmp.singleMessageHandler == nil {
+	if cmp.onEachMessageHandler == nil {
 		return errors.New("you must define a MessageHandler first")
 	}
 
 	unrecoverableError := make(chan error)
 	go func() {
 		for {
-			message, err := consumer.ReadMessage(cmp.serverCtx)
+			message, err := consumer.FetchMessage(cmp.ServerCtx)
 			if err != nil {
 				cmp.consumptionErrors <- err
-				cmp.logger.Error("encountered an error while reading message", elog.FieldErr(err))
+				cmp.logger.Error("encountered an error while fetching message", elog.FieldErr(err))
 
-				if kafkaError, ok := err.(kafka.Error); ok {
-					// If this error is unrecoverable, stop consuming.
-					if kafkaError.Temporary() == false {
-						unrecoverableError <- kafkaError
-						return
-					}
+				// If this error is unrecoverable, stop consuming.
+				if isErrorUnrecoverable(err) == false {
+					unrecoverableError <- err
+					return
 				}
 			}
 
-			if cmp.serverCtx.Err() != nil {
+			if cmp.ServerCtx.Err() != nil {
 				return
 			}
 
-			err = cmp.singleMessageHandler(message)
+			err = cmp.onEachMessageHandler(cmp.ServerCtx, message)
 			if err != nil {
 				cmp.logger.Error("encountered an error while handling message", elog.FieldErr(err))
 				cmp.consumptionErrors <- err
+			}
+
+			err = consumer.CommitMessages(cmp.ServerCtx, message)
+			if err != nil {
+				cmp.consumptionErrors <- err
+				cmp.logger.Error("encountered an error while committing message", elog.FieldErr(err))
+
+				// If this error is unrecoverable, stop consuming.
+				if isErrorUnrecoverable(err) == false {
+					unrecoverableError <- err
+					return
+				}
 			}
 		}
 	}()
 
 	select {
-	case <-cmp.serverCtx.Done():
-		rootErr := cmp.serverCtx.Err()
+	case <-cmp.ServerCtx.Done():
+		rootErr := cmp.ServerCtx.Err()
 		cmp.logger.Error("terminating consumer because a context error", elog.FieldErr(rootErr))
 
 		err := cmp.closeConsumer(consumer)
@@ -146,19 +222,19 @@ func (cmp *Component) startSingleMode() error {
 		}
 
 		return rootErr
-	case rootErr := <-unrecoverableError:
-		if rootErr == nil {
+	case originErr := <-unrecoverableError:
+		if originErr == nil {
 			panic("unrecoverableError should receive an error instead of nil")
 		}
 
-		cmp.logger.Fatal("stopping server because of an unrecoverable error", elog.FieldErr(rootErr))
+		cmp.logger.Fatal("stopping server because of an unrecoverable error", elog.FieldErr(originErr))
 		cmp.Stop()
 
 		err := cmp.closeConsumer(consumer)
 		if err != nil {
 			return fmt.Errorf("exiting due to an unrecoverable error, but encountered an error while closing consumer: %w", err)
 		}
-		return rootErr
+		return originErr
 	}
 }
 
@@ -175,12 +251,12 @@ func (cmp *Component) closeConsumer(consumer *ekafka.Consumer) error {
 func NewConsumerServerComponent(name string, config *config, ekafkaComponent *ekafka.Component, logger *elog.Component) *Component {
 	serverCtx, stopServer := context.WithCancel(context.Background())
 	return &Component{
+		ServerCtx:       serverCtx,
+		stopServer:      stopServer,
 		name:            name,
 		config:          config,
 		ekafkaComponent: ekafkaComponent,
 		logger:          logger,
-		serverCtx:       serverCtx,
-		stopServer:      stopServer,
 		mode:            consumptionModeSingle,
 	}
 }
