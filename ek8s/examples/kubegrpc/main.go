ed9a8390186afed6230e4fe7fc8d2cd500079c67
askuy <askuygo@foxmail.com>
2021-04-06 14:49:35 +0800

    upgrade k8s client

--- ek8s/examples/kubegrpc/main.go
+++ ek8s/examples/kubegrpc/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/gotomicro/ego"
+	"github.com/gotomicro/ego/client/egrpc"
+	"github.com/gotomicro/ego/core/elog"
+
 	"github.com/gotomicro/ego-component/ek8s"
 	"github.com/gotomicro/ego-component/ek8s/examples/kubegrpc/helloworld"
 	"github.com/gotomicro/ego-component/ek8s/registry"
-	"github.com/gotomicro/ego/client/egrpc"
-	"github.com/gotomicro/ego/client/egrpc/resolver"
-	"github.com/gotomicro/ego/core/elog"
 )
 
 func main() {
@@ -20,7 +21,7 @@ func main() {
 }
 
 func invokerGrpc() error {
-	resolver.Register("k8s",  registry.Load("registry").Build(registry.WithClientK8s(ek8s.Load("k8s").Build())))
+	registry.Load("registry").Build(registry.WithClientK8s(ek8s.Load("k8s").Build()))
 	grpcConn := egrpc.Load("grpc.test").Build()
 	grpcComp := helloworld.NewGreeterClient(grpcConn.ClientConn)
 	fmt.Printf("client--------------->"+"%+v\n", grpcComp)
