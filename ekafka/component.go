2965386a79a97e98f355cd2fc51ec0eda7cc11f2
AaronJan <aaronjan@qq.com>
2021-04-19 21:39:48 +0800

    fix: fix race condition

--- ekafka/component.go
+++ ekafka/component.go
@@ -17,9 +17,9 @@ type Component struct {
 	client     *Client
 	consumers  map[string]*Consumer
 	producers  map[string]*Producer
-	clientMu   sync.Mutex
-	consumerMu sync.Mutex
-	producerMu sync.Mutex
+	clientOnce sync.Once
+	consumerMu sync.RWMutex
+	producerMu sync.RWMutex
 }
 
 func (cmp *Component) interceptorChain() func(oldProcess processFn) processFn {
@@ -28,125 +28,132 @@ func (cmp *Component) interceptorChain() func(oldProcess processFn) processFn {
 
 // Producer 返回指定名称的kafka Producer
 func (cmp *Component) Producer(name string) *Producer {
+	cmp.producerMu.RLock()
+
 	if producer, ok := cmp.producers[name]; ok {
+		cmp.producerMu.RUnlock()
 		return producer
 	}
 
+	cmp.producerMu.RUnlock()
 	cmp.producerMu.Lock()
-	defer cmp.producerMu.Unlock()
 
-	if _, ok := cmp.producers[name]; !ok {
-		config, ok := cmp.config.Producers[name]
-		if !ok {
-			cmp.logger.Panic("producer config not exists", elog.String("name", name))
-		}
-
-		// 如果未设置balancer，则使用roundRobin
-		if config.Balancer == "" {
-			config.Balancer = balancerRoundRobin
-		}
-		balancer, ok := cmp.config.balancers[config.Balancer]
-		if !ok {
-			panic(fmt.Sprintf(
-				"producer.Balancer is not in registered balancers, %s, %v",
-				config.Balancer,
-				cmp.config.balancers,
-			))
-		}
-		producer := &Producer{
-			w: &kafka.Writer{
-				Addr:         kafka.TCP(cmp.config.Brokers...),
-				Topic:        config.Topic,
-				Balancer:     balancer,
-				MaxAttempts:  config.MaxAttempts,
-				BatchSize:    config.BatchSize,
-				BatchBytes:   config.BatchBytes,
-				BatchTimeout: config.BatchTimeout,
-				ReadTimeout:  config.ReadTimeout,
-				WriteTimeout: config.WriteTimeout,
-				RequiredAcks: config.RequiredAcks,
-				Async:        config.Async,
-			},
-			processor: defaultProcessor,
-			logMode:   cmp.config.Debug,
-		}
-		producer.wrapProcessor(cmp.interceptorChain())
-
-		cmp.producers[name] = producer
+	if producer, ok := cmp.producers[name]; ok {
+		cmp.producerMu.Unlock()
+		return producer
 	}
+
+	config, ok := cmp.config.Producers[name]
+	if !ok {
+		cmp.logger.Panic("producer config not exists", elog.String("name", name))
+	}
+	// 如果未设置balancer，则使用roundRobin
+	if config.Balancer == "" {
+		config.Balancer = balancerRoundRobin
+	}
+	balancer, ok := cmp.config.balancers[config.Balancer]
+	if !ok {
+		cmp.producerMu.Unlock()
+		panic(fmt.Sprintf(
+			"producer.Balancer is not in registered balancers, %s, %v",
+			config.Balancer,
+			cmp.config.balancers,
+		))
+	}
+	producer := &Producer{
+		w: &kafka.Writer{
+			Addr:         kafka.TCP(cmp.config.Brokers...),
+			Topic:        config.Topic,
+			Balancer:     balancer,
+			MaxAttempts:  config.MaxAttempts,
+			BatchSize:    config.BatchSize,
+			BatchBytes:   config.BatchBytes,
+			BatchTimeout: config.BatchTimeout,
+			ReadTimeout:  config.ReadTimeout,
+			WriteTimeout: config.WriteTimeout,
+			RequiredAcks: config.RequiredAcks,
+			Async:        config.Async,
+		},
+		processor: defaultProcessor,
+		logMode:   cmp.config.Debug,
+	}
+	producer.wrapProcessor(cmp.interceptorChain())
+	cmp.producers[name] = producer
+
+	cmp.producerMu.Unlock()
 
 	return cmp.producers[name]
 }
 
 // Consumer 返回指定名称的kafka Consumer
 func (cmp *Component) Consumer(name string) *Consumer {
+	cmp.consumerMu.RLock()
+
 	if consumer, ok := cmp.consumers[name]; ok {
+		cmp.consumerMu.RUnlock()
 		return consumer
 	}
 
+	cmp.consumerMu.RUnlock()
 	cmp.consumerMu.Lock()
-	defer cmp.consumerMu.Unlock()
 
-	if _, ok := cmp.consumers[name]; !ok {
-		config, ok := cmp.config.Consumers[name]
-		if !ok {
-			cmp.logger.Panic("consumer config not exists", elog.String("name", name))
-		}
-
-		logger := newKafkaLogger(cmp.logger)
-		errorLogger := newKafkaErrorLogger(cmp.logger)
-		consumer := &Consumer{
-			r: kafka.NewReader(kafka.ReaderConfig{
-				Brokers:                cmp.config.Brokers,
-				Topic:                  config.Topic,
-				GroupID:                config.GroupID,
-				Partition:              config.Partition,
-				MinBytes:               config.MinBytes,
-				MaxBytes:               config.MaxBytes,
-				WatchPartitionChanges:  config.WatchPartitionChanges,
-				PartitionWatchInterval: config.PartitionWatchInterval,
-				RebalanceTimeout:       config.RebalanceTimeout,
-				MaxWait:                config.MaxWait,
-				ReadLagInterval:        config.ReadLagInterval,
-				Logger:                 logger,
-				ErrorLogger:            errorLogger,
-				HeartbeatInterval:      config.HeartbeatInterval,
-				CommitInterval:         config.CommitInterval,
-				SessionTimeout:         config.SessionTimeout,
-				JoinGroupBackoff:       config.JoinGroupBackoff,
-				RetentionTime:          config.RetentionTime,
-				StartOffset:            config.StartOffset,
-				ReadBackoffMin:         config.ReadBackoffMin,
-				ReadBackoffMax:         config.ReadBackoffMax,
-			}),
-			processor: defaultProcessor,
-			logMode:   cmp.config.Debug,
-		}
-		consumer.wrapProcessor(cmp.interceptorChain())
-
-		cmp.consumers[name] = consumer
+	if consumer, ok := cmp.consumers[name]; ok {
+		cmp.consumerMu.Unlock()
+		return consumer
 	}
+
+	config, ok := cmp.config.Consumers[name]
+	if !ok {
+		cmp.consumerMu.Unlock()
+		cmp.logger.Panic("consumer config not exists", elog.String("name", name))
+	}
+	logger := newKafkaLogger(cmp.logger)
+	errorLogger := newKafkaErrorLogger(cmp.logger)
+	consumer := &Consumer{
+		r: kafka.NewReader(kafka.ReaderConfig{
+			Brokers:                cmp.config.Brokers,
+			Topic:                  config.Topic,
+			GroupID:                config.GroupID,
+			Partition:              config.Partition,
+			MinBytes:               config.MinBytes,
+			MaxBytes:               config.MaxBytes,
+			WatchPartitionChanges:  config.WatchPartitionChanges,
+			PartitionWatchInterval: config.PartitionWatchInterval,
+			RebalanceTimeout:       config.RebalanceTimeout,
+			MaxWait:                config.MaxWait,
+			ReadLagInterval:        config.ReadLagInterval,
+			Logger:                 logger,
+			ErrorLogger:            errorLogger,
+			HeartbeatInterval:      config.HeartbeatInterval,
+			CommitInterval:         config.CommitInterval,
+			SessionTimeout:         config.SessionTimeout,
+			JoinGroupBackoff:       config.JoinGroupBackoff,
+			RetentionTime:          config.RetentionTime,
+			StartOffset:            config.StartOffset,
+			ReadBackoffMin:         config.ReadBackoffMin,
+			ReadBackoffMax:         config.ReadBackoffMax,
+		}),
+		processor: defaultProcessor,
+		logMode:   cmp.config.Debug,
+	}
+	consumer.wrapProcessor(cmp.interceptorChain())
+	cmp.consumers[name] = consumer
+
+	cmp.consumerMu.Unlock()
 
 	return cmp.consumers[name]
 }
 
 // Client 返回kafka Client
 func (cmp *Component) Client() *Client {
-	if cmp.client != nil {
-		return cmp.client
-	}
-
-	cmp.clientMu.Lock()
-	defer cmp.clientMu.Unlock()
-
-	if cmp.client == nil {
+	cmp.clientOnce.Do(func() {
 		cmp.client = &Client{
 			cc:        &kafka.Client{Addr: kafka.TCP(cmp.config.Brokers...), Timeout: cmp.config.Client.Timeout},
 			processor: defaultProcessor,
 			logMode:   cmp.config.Debug,
 		}
 		cmp.client.wrapProcessor(cmp.interceptorChain())
-	}
+	})
 
 	return cmp.client
 }
