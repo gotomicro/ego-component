565f924ae167764673496fcd464d8d95150d2469
soeluc <511576209@qq.com>
2022-03-28 13:24:47 +0800

    fix：  PublishMsg when qos = 0 An error occurred

--- checkgobuild.sh
+++ checkgobuild.sh
@@ -1,4 +1,4 @@
-TARGET_DIR=("ealiyun" "edingtalk" "ees" "eetcd" "egitlab" "egorm" "ehuawei" "ejenkins" "ejira" "ek8s" "ekafka" "elogger" "eminio" "emns" "emongo" "eoauth2" "eredis" "esession" "etoken" "ewechat")
+TARGET_DIR=("ealiyun" "edingtalk" "ees" "eetcd" "egitlab" "egorm" "ehuawei" "ejenkins" "ejira" "ek8s" "ekafka" "elogger" "eminio" "emns" "emongo" "eoauth2" "eredis" "esession" "etoken" "ewechat" "eemqtt")
 ROOT=$(pwd)
 for dir in ${TARGET_DIR[@]}; do
     cd $dir && go build -v ./...
